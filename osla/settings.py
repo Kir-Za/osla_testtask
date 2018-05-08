b917e90f1a212587203637bb0b2201ec7ee598b3
kir <zarechenec@osla.ru>
2018-05-08 11:57:43 +0300

    dev start point

--- osla/settings.py
+++ osla/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't54$sv53-8-s3(tjs!@crwfcq_$2l@5s&^3_vrr#whkf!fee91'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = ['demo-osla.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com', 'demo-osla.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
